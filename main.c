299dded5ec7897ba4c4336b1153c42e549d8734b
tracer <tracer@tracer.org>
2018-06-10 14:58:30 +0800

    >  compile cyh Linux 161240005 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- main.c
+++ main.c
@@ -1,15 +1,38 @@
 #include "kvdb.h"
 #include <stdlib.h>
 
+#define M 100
+
 int main() {
   kvdb_t db;
   const char *key = "operating-systems";
+  const char *test= "case" ;
   char *value;
 
   kvdb_open(&db, "a.db");
   kvdb_put(&db, key, "three-easy-pieces");
+  
+  char str[M] = {0};  
+  int i = 0;  
+  char symbol[4] = {'|','/','-','\\'};  
+  for(i = 0; i<M; ++i){  
+      int c = i%5;  
+      switch(c){  
+            case 0:printf(RED);    break;  
+            case 1:printf(GREEN);  break;  
+            case 2:printf(BLUE);   break;  
+            case 3:printf(YELLOW); break;  
+            case 4:printf(BROWN);  break;  
+     }  
+     kvdb_put(&db,test,str);
+     str[i] = symbol[c];
+     str[i+1] = '\0';  
+  }  
   value = kvdb_get(&db, key);
   printf("[%s]: [%s]\n", key, value);
+  value = kvdb_get(&db, test);
+  printf("[%s]: [%s]\n", test, value);
   free(value);
   return 0;
 }
+
