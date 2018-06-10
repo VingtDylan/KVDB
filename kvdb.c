684184d91f899d2681c2e5c8b6d0cbe1e2cfbc06
tracer <tracer@tracer.org>
2018-06-10 14:47:10 +0800

    >  compile cyh Linux 161240005 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- kvdb.c
+++ kvdb.c
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <sys/file.h>
 #include <unistd.h>
+
 //static char key[LENGTH];
 //static char value[LENGTH];
 //static char input[LENGTH];
@@ -115,8 +116,7 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value) {
   return ret;
 }
 
-char *kvdb_get(kvdb_t *db, const char *key) {
-  // BUG: no error checking
+static char *unsafe_get(kvdb_t *db,const char *key){
   static char _key[1 << 20], _value[1 << 20];
   char *ret = NULL;
   fseek(db->fp, 0, SEEK_SET);
@@ -129,6 +129,34 @@ char *kvdb_get(kvdb_t *db, const char *key) {
       if (!ret) free(ret);
       ret = strdup(_value);
     }
+  }
+  return ret;
+}
+
+char *kvdb_get(kvdb_t *db, const char *key) {
+  // BUG: no error checking
+  //static char _key[1 << 20], _value[1 << 20];
+  //char *ret = NULL;
+  //fseek(db->fp, 0, SEEK_SET);
+  //while (1) {
+    //if (!fgets(_key, sizeof(_key), db->fp)) break;
+    //if (!fgets(_value, sizeof(_value), db->fp)) break;
+    //_key[strlen(_key) - 1] = '\0';
+    //_value[strlen(_value) - 1] = '\0';
+    //if (strcmp(key, _key) == 0) {
+      //if (!ret) free(ret);
+      //ret = strdup(_value);
+    //}
+  //}
+  //return ret;
+  if(pthread_mutex_lock(&db->mutex)!=0){
+     perror("get lock");
+     return NULL;
+  }
+  char *ret=unsafe_get(db,key);
+  if(pthread_mutex_unlock(&db->mutex)!=0){
+     perror("get unlock");
+     return NULL;
   }
   return ret;
 }
